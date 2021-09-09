4bf99894b935eab00a9b65ad4ee4eeef23e9348e
greenrus <89820986+greenrus@users.noreply.github.com>
2021-09-09 12:50:22 +0900

    Update Q1.java
    edit

--- Q1.java
+++ Q1.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 public class Q1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		double[] avg = new double[6];			
+		double[] avg = { 85.6, 79.5, 83.1, 80.0, 78.2, 75.0 };			
 		
-		for (int i = 0; i < avg.length; i++) {
-			avg[i] = sc.nextDouble();
-		}
 		int idx1 = sc.nextInt() - 1;
 		int idx2 = sc.nextInt() - 1;
 		
